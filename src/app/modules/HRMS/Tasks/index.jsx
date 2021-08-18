58dde9c91c5a5ed42072adf5591267588f2420cd
munawer <munawer.ali@contentcreation.com.my>
2021-08-18 20:09:23 +0500

    update

--- src/app/modules/HRMS/Tasks/index.jsx
+++ src/app/modules/HRMS/Tasks/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Row, Col } from 'antd';
 import { useTranslate } from 'Translate';
 import CardListSwitchLayout from '../../../molecules/HRMS/CardListSwitchLayout';
